ec952c155e979a2418dfe9f1b24a2e723e7436e4
Rui Wang <wruits@cn.ibm.com>
2025-09-30 00:29:39 +0800

    delete

--- backend-services/user-service/src/main/java/com/ai/qa/user/api/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.ai.qa.user.api.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String username;
-    private String password;
-}
--- backend-services/user-service/src/main/java/com/ai/qa/user/application/userService.java
+++
@@ -1,8 +0,0 @@
-package com.ai.qa.user.application;
-
-public interface userService {
-
-//    User login(String username,String pwd);
-
-//    User register(String username,String pwd);
-}
